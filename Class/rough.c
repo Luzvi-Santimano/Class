daaf989f0471a73ed304024056fb4261d9331895
Luzvi-Santimano <luzvisantimano@gmail.com>
2022-01-20 18:55:46 +0530

    operator assignment

---
+++ Class/rough.c
@@ -0,0 +1,21 @@
+/*
+ *
+ * 7. Write a program to rotate the bits. Input the no.of rotations, at runtime.
+	Ex : binary : 10000000000000000000000000001011
+	rotations : suppose 3 times right, then
+	result : 01110000000000000000000000000001
+	binary : 10000000000000000000000000001011
+	rotations : suppose 4 times left, then
+	result : 0000000000000000000000001011100
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("\nHello world\n");
+	return 0;
+}
+
+
